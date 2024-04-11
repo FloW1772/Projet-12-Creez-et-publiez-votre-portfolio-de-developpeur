f2e7929215c66fa378cecf580b769cdb74f76866
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-11 19:17:11 +0200

    update projectCARD

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -8,7 +8,7 @@ export default function ProjectCard({ id, title, description, cover, link }) {
       <div className="project-info">
         <h2 className="project-title">{title}</h2>
         <p className="project-description">{description}</p>
-        <a href={link} className="project-link">Voir le projet</a>
+        <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet sur GitHub</a>
       </div>
     </div>
   );
