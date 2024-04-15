6549eeae0287e950470b58ed84934b2de56e46fa
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-15 17:45:52 +0200

    fix portfolio

--- src/pages/Porfolio/Portfolio.jsx
+++ src/pages/Porfolio/Portfolio.jsx
@@ -114,7 +114,41 @@ export default function Portfolio() {
       description: 'Ohmyfood est un site web "mobile first" qui répertorie les menus de restaurants gastronomiques. En plus des systèmes classiques de réservation, les clients pourront composer le menu de leur repas pour que les plats soient prêts à leur arrivée. Finis, les temps d attente au restaurant !',
       link: 'https://github.com/FloW1772/Projet-4-Ohmyfood',
     },
-    // Ajoutez les autres projets ici...
+    {
+            id: 2,
+            cover: Print,
+            title: 'Projet-5-Print-it',
+            description: 'Voici le site internet statique d une petite entreprise qu il a fallu dynamiser',
+            link: 'https://github.com/FloW1772/Projet-5-Print-it',
+          },
+          {
+            id: 3,
+            cover: sophie,
+            title: 'Projet-6-Sophie-Bluel',
+            description: 'Le site web est un portfolio de création d un architecte d intérieur',
+            link: 'https://github.com/FloW1772/Projet-6-Sophie-Bluel',
+          },
+          {
+            id: 4,
+            cover: Kasa,
+            title: 'Projet-8-Kasa',
+            description: 'Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, il a donc fallu faire une refonte totale avec une stack complète en JavaScript avec NodeJS côté back-end, et React côté front-end.',
+            link: 'https://github.com/FloW1772/Projet-8-Kasa',
+          },
+          {
+            id: 5,
+            cover: nina,
+            title: 'Projet-9-Nina-Carducci',
+            description: 'J ai découvert se site après de la prospection et il nécessitait mais service d optimisation SEO',
+            link: 'https://github.com/FloW1772/Projet-9-Nina-Carducci',
+          },
+          {
+            id: 6,
+            cover: events,
+            title: 'Projet-10-724events',
+            description: 'l agence évènementielle 724events vous contacte pour une mission L agence souhaite publier la nouvelle version de son site vitrine. Il s agit d un site one-page pas a 100% finalisé il y a donc des bugs a régler',
+            link: 'https://github.com/FloW1772/Projet-10-724events',
+          },
   ];
 
   return (
