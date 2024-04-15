fea36c19981b3d2fba47211dbc60a51a0200d33f
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-15 13:21:05 +0200

    tentative de fix la modale

--- src/components/Modale/Modale.jsx
+++ src/components/Modale/Modale.jsx
@@ -70,6 +70,84 @@
 //     </div>
 //   );
 // }
+// import React, { useState } from 'react';
+// import './modale.scss';
+
+// export default function Modal({ closeModal, addProject }) {
+//   const [newProject, setNewProject] = useState({
+//     cover: '',
+//     title: '',
+//     description: '',
+//     link: '',
+//   });
+
+//   // Fonction pour gérer les changements dans les champs du formulaire
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setNewProject({ ...newProject, [name]: value });
+//   };
+
+//   // Fonction pour gérer le changement de fichier
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     setNewProject({ ...newProject, cover: file });
+//   };
+
+//   // Fonction pour soumettre le formulaire
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Vérification que tous les champs sont remplis
+//     if (newProject.cover && newProject.title && newProject.description && newProject.link) {
+//       // Vérification que le lien commence par "https://github.com"
+//       const githubRegex = /^https:\/\/github\.com/;
+//       if (!githubRegex.test(newProject.link)) {
+//         alert("Le lien doit commencer par https://github.com");
+//         return; // Arrêter le traitement si le lien n'est pas valide
+//       }
+//       // Vérification que le fichier sélectionné est une image
+//       if (!newProject.cover.type.startsWith('image/')) {
+//         alert("Veuillez sélectionner un fichier image.");
+//         return; // Arrêter le traitement si le fichier n'est pas une image
+//       }
+//       // Appel de la fonction addProject pour ajouter le nouveau projet
+//       addProject(newProject);
+//       // Réinitialisation du formulaire et fermeture de la modale
+//       setNewProject({ cover: '', title: '', description: '', link: '' });
+//       closeModal();
+//     } else {
+//       alert('Veuillez remplir tous les champs.');
+//     }
+//   };
+
+//   return (
+//     <div className="modal-overlay">
+//       <div className="modal-content">
+//         <span className="close-modal" onClick={closeModal}>X</span>
+//         <h2>Ajouter un nouveau projet</h2>
+//         <form onSubmit={handleSubmit}>
+//           <div className="form-group">
+//             <label htmlFor="cover">Image du projet:</label>
+//             <input type="file" id="cover" name="cover" onChange={handleFileChange} />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="title">Titre:</label>
+//             <input type="text" id="title" name="title" value={newProject.title} onChange={handleChange} />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="description">Description:</label>
+//             <input type="text" id="description" name="description" value={newProject.description} onChange={handleChange}/>
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="link">Lien GitHub:</label>
+//             <input type="text" id="link" name="link" value={newProject.link} onChange={handleChange} />
+//           </div>
+//           <button type="submit">Ajouter</button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
 import React, { useState } from 'react';
 import './modale.scss';
 
@@ -110,7 +188,7 @@ export default function Modal({ closeModal, addProject }) {
         return; // Arrêter le traitement si le fichier n'est pas une image
       }
       // Appel de la fonction addProject pour ajouter le nouveau projet
-      addProject(newProject);
+      addProject(newProject); // Utiliser addProject ici
       // Réinitialisation du formulaire et fermeture de la modale
       setNewProject({ cover: '', title: '', description: '', link: '' });
       closeModal();
@@ -135,7 +213,7 @@ export default function Modal({ closeModal, addProject }) {
           </div>
           <div className="form-group">
             <label htmlFor="description">Description:</label>
-            <textarea id="description" name="description" value={newProject.description} onChange={handleChange}></textarea>
+            <input type="text" id="description" name="description" value={newProject.description} onChange={handleChange}/>
           </div>
           <div className="form-group">
             <label htmlFor="link">Lien GitHub:</label>
--- src/pages/Porfolio/Portfolio.jsx
+++ src/pages/Porfolio/Portfolio.jsx
@@ -1,3 +1,86 @@
+// import React, { useState } from 'react';
+// import ProjectCard from '../../components/ProjectCard/ProjectCard';
+// import Modale from '../../components/Modale/Modale'
+// import events from '../../assets/logo/events.png';
+// import Kasa from '../../assets/logo/Kasa.png';
+// import nina from '../../assets/logo/nina.webp';
+// import ohmyfood from '../../assets/logo/ohmyfood.png';
+// import Print from '../../assets/logo/Print-it.png';
+// import sophie from '../../assets/logo/sophie-bluel.png';
+
+// export default function Portfolio() {
+
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   const toggleModal = () => {
+//     setIsModalOpen(!isModalOpen);
+//   };
+//   const projects = [
+//     {
+//       id: 1,
+//       cover: ohmyfood,
+//       title: 'Projet-4-Ohmyfood',
+//       description: 'Ohmyfood est un site web "mobile first" qui répertorie les menus de restaurants gastronomiques. En plus des systèmes classiques de réservation, les clients pourront composer le menu de leur repas pour que les plats soient prêts à leur arrivée. Finis, les temps d attente au restaurant !',
+//       link: 'https://github.com/FloW1772/Projet-4-Ohmyfood',
+//     },
+//     {
+//       id: 2,
+//       cover: Print,
+//       title: 'Projet-5-Print-it',
+//       description: 'Voici le site internet statique d une petite entreprise qu il a fallu dynamiser',
+//       link: 'https://github.com/FloW1772/Projet-5-Print-it',
+//     },
+//     {
+//       id: 3,
+//       cover: sophie,
+//       title: 'Projet-6-Sophie-Bluel',
+//       description: 'Le site web est un portfolio de création d un architecte d intérieur',
+//       link: 'https://github.com/FloW1772/Projet-6-Sophie-Bluel',
+//     },
+//     {
+//       id: 4,
+//       cover: Kasa,
+//       title: 'Projet-8-Kasa',
+//       description: 'Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, il a donc fallu faire une refonte totale avec une stack complète en JavaScript avec NodeJS côté back-end, et React côté front-end.',
+//       link: 'https://github.com/FloW1772/Projet-8-Kasa',
+//     },
+//     {
+//       id: 5,
+//       cover: nina,
+//       title: 'Projet-9-Nina-Carducci',
+//       description: 'J ai découvert se site après de la prospection et il nécessitait mais service d optimisation SEO',
+//       link: 'https://github.com/FloW1772/Projet-9-Nina-Carducci',
+//     },
+//     {
+//       id: 6,
+//       cover: events,
+//       title: 'Projet-10-724events',
+//       description: 'l agence évènementielle 724events vous contacte pour une mission L agence souhaite publier la nouvelle version de son site vitrine. Il s agit d un site one-page pas a 100% finalisé il y a donc des bugs a régler',
+//       link: 'https://github.com/FloW1772/Projet-10-724events',
+//     },
+//   ];
+
+// return (
+//   <div>
+//     <h1 className="portfolio-title">Portfolio</h1>
+//     <div className="project-cards">
+//       {projects.map((project) => (
+//         <ProjectCard
+//           key={project.id}
+//           id={project.id}
+//           title={project.title}
+//           description={project.description}
+//           cover={project.cover}
+//           link={project.link}
+//         />
+//       ))}
+//     </div>
+//     <button onClick={toggleModal}>Ajouter un nouveau projet</button>
+//     {isModalOpen && <Modale onClose={toggleModal} />} {/* Affiche la modal si isModalOpen est vrai */}
+//   </div>
+// );
+// }
+
 import React, { useState } from 'react';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import Modale from '../../components/Modale/Modale'
@@ -15,6 +98,16 @@ export default function Portfolio() {
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+
+  const addProject = (newProject) => {
+    // Logique pour ajouter le nouveau projet
+    console.log('Nouveau projet ajouté:', newProject);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   const projects = [
     {
       id: 1,
@@ -23,78 +116,26 @@ export default function Portfolio() {
       description: 'Ohmyfood est un site web "mobile first" qui répertorie les menus de restaurants gastronomiques. En plus des systèmes classiques de réservation, les clients pourront composer le menu de leur repas pour que les plats soient prêts à leur arrivée. Finis, les temps d attente au restaurant !',
       link: 'https://github.com/FloW1772/Projet-4-Ohmyfood',
     },
-    {
-      id: 2,
-      cover: Print,
-      title: 'Projet-5-Print-it',
-      description: 'Voici le site internet statique d une petite entreprise qu il a fallu dynamiser',
-      link: 'https://github.com/FloW1772/Projet-5-Print-it',
-    },
-    {
-      id: 3,
-      cover: sophie,
-      title: 'Projet-6-Sophie-Bluel',
-      description: 'Le site web est un portfolio de création d un architecte d intérieur',
-      link: 'https://github.com/FloW1772/Projet-6-Sophie-Bluel',
-    },
-    {
-      id: 4,
-      cover: Kasa,
-      title: 'Projet-8-Kasa',
-      description: 'Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, il a donc fallu faire une refonte totale avec une stack complète en JavaScript avec NodeJS côté back-end, et React côté front-end.',
-      link: 'https://github.com/FloW1772/Projet-8-Kasa',
-    },
-    {
-      id: 5,
-      cover: nina,
-      title: 'Projet-9-Nina-Carducci',
-      description: 'J ai découvert se site après de la prospection et il nécessitait mais service d optimisation SEO',
-      link: 'https://github.com/FloW1772/Projet-9-Nina-Carducci',
-    },
-    {
-      id: 6,
-      cover: events,
-      title: 'Projet-10-724events',
-      description: 'l agence évènementielle 724events vous contacte pour une mission L agence souhaite publier la nouvelle version de son site vitrine. Il s agit d un site one-page pas a 100% finalisé il y a donc des bugs a régler',
-      link: 'https://github.com/FloW1772/Projet-10-724events',
-    },
+    // Ajoutez les autres projets ici...
   ];
 
-//   return (
-//     <div>
-//       <h1 className="portfolio-title">Portfolio</h1>
-//       <div className="project-cards">
-//         {projects.map((project) => (
-//           <ProjectCard
-//             key={project.id}
-//             id={project.id}
-//             title={project.title}
-//             description={project.description}
-//             cover={project.cover}
-//             link={project.link}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-return (
-  <div>
-    <h1 className="portfolio-title">Portfolio</h1>
-    <div className="project-cards">
-      {projects.map((project) => (
-        <ProjectCard
-          key={project.id}
-          id={project.id}
-          title={project.title}
-          description={project.description}
-          cover={project.cover}
-          link={project.link}
-        />
-      ))}
+  return (
+    <div>
+      <h1 className="portfolio-title">Portfolio</h1>
+      <div className="project-cards">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            id={project.id}
+            title={project.title}
+            description={project.description}
+            cover={project.cover}
+            link={project.link}
+          />
+        ))}
+      </div>
+      <button onClick={toggleModal}>Ajouter un nouveau projet</button>
+      {isModalOpen && <Modale onClose={closeModal} addProject={addProject} />} {/* Passer closeModal comme prop */}
     </div>
-    <button onClick={toggleModal}>Ajouter un nouveau projet</button>
-    {isModalOpen && <Modale onClose={toggleModal} />} {/* Affiche la modal si isModalOpen est vrai */}
-  </div>
-);
+  );
 }
