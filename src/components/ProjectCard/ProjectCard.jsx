b8178ae8a7fad845b1f5bc0b000a29fb141dc10f
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-18 01:36:04 +0200

    tentative de fix

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,35 +1,15 @@
-// import React from 'react';
-// import './ProjectCard.scss';
-
-// export default function ProjectCard({ id, title, description, cover, link }) {
-//   return (
-//     <div className="project-card">
-//       <img src={cover} alt={title} className="project-cover" />
-//       <div className="project-info">
-//         <h2 className="project-title">{title}</h2>
-//         <p className="project-description">{description}</p>
-//         <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet sur GitHub</a>
-//       </div>
-//     </div>
-//   );
-// }
 import React from 'react';
+import './ProjectCard.scss';
 
-const ProjectCard = ({ id, title, description, cover, link }) => {
-  const ouvrirLienGitHub = () => {
-    window.open(link, '_blank');
-  };
-
+export default function ProjectCard({ id, title, description, cover, link }) {
   return (
     <div className="project-card">
       <img src={cover} alt={title} className="project-cover" />
       <div className="project-info">
         <h2 className="project-title">{title}</h2>
         <p className="project-description">{description}</p>
-        <button onClick={ouvrirLienGitHub}>Voir sur GitHub</button>
+        <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet sur GitHub</a>
       </div>
     </div>
   );
-};
-
-export default ProjectCard;
+}
