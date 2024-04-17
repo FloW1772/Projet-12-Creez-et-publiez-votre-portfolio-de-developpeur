3caf86b08cc34ea380029d10d4040000571759da
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-17 19:26:11 +0200

    update porfolio

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import './ProjectCard.scss';
+// import React from 'react';
+// import './ProjectCard.scss';
 
-export default function ProjectCard({ id, title, description, cover, link }) {
+// export default function ProjectCard({ id, title, description, cover, link }) {
+//   return (
+//     <div className="project-card">
+//       <img src={cover} alt={title} className="project-cover" />
+//       <div className="project-info">
+//         <h2 className="project-title">{title}</h2>
+//         <p className="project-description">{description}</p>
+//         <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet sur GitHub</a>
+//       </div>
+//     </div>
+//   );
+// }
+import React from 'react';
+
+const ProjectCard = ({ id, title, description, cover, link }) => {
+  const ouvrirLienGitHub = () => {
+    window.open(link, '_blank');
+  };
+
   return (
     <div className="project-card">
       <img src={cover} alt={title} className="project-cover" />
       <div className="project-info">
         <h2 className="project-title">{title}</h2>
         <p className="project-description">{description}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet sur GitHub</a>
+        <button onClick={ouvrirLienGitHub}>Voir sur GitHub</button>
       </div>
     </div>
   );
-}
+};
+
+export default ProjectCard;
