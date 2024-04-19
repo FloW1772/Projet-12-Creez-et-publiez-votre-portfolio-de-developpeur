530e1c6953808c0458bab582b60abe3ca5990403
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-19 05:15:00 +0200

    fix competence

--- src/components/competences/Competences.jsx
+++ src/components/competences/Competences.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importer Link depuis react-router-dom
 import './competences.scss';
 
 const Competences = ({ competences }) => {
   return (
     <div className="competences">
-      <h2>Compétences</h2>
+      <h2>Compétences :</h2>
+      
+        <p>Mes compétences se sont développées. Grâce au travail effectué sur différents projets pour voir mes projets, c'est <Link to="/portfolio"> <strong>ici.</strong></Link>.</p>
+      
       <div className="cards-container">
         {competences.map((competence, index) => (
           <div className="card" key={index}>
