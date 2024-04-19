c65082ac3b2c25075fd134c78f6db521d9f8267c
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-19 04:04:24 +0200

    update competence

--- src/components/competences/Competences.jsx
+++ src/components/competences/Competences.jsx
@@ -12,6 +12,11 @@ const Competences = ({ competences }) => {
           </div>
         ))}
       </div>
+      <div className="bottom-text">
+        <p>Mon style vous plaît ?</p>
+        <p>N'hésitez pas à me contacter, je me ferai un plaisir d'échanger avec vous, et c'est sans engagement de votre part !</p>
+        <p><a href="/contact" className="button">Contactez-moi</a></p>
+      </div>
     </div>
   );
 };
