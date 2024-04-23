58582021e2f8dcd2e87d4bf07d3c3f94e91f3859
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-23 19:52:07 +0200

    mise en place component presentation

--- src/components/Projet/Projet.jsx
+++ src/components/Projet/Projet.jsx
@@ -1,16 +1,35 @@
+// import React from 'react';
+// import './projet.scss';
+// import Image1 from '../../assets/images/Image1.png'
+// import Image2 from '../../assets/images/Image2.png'
+// import Image3 from '../../assets/images/Image3.png'
+// import Image4 from '../../assets/images/Image4.png'
+// import Image5 from '../../assets/images/Image5.png'
+// import Image6 from '../../assets/images/Image6.png'
+// import Image7 from '../../assets/images/Image7.png'
+// import Image8 from '../../assets/images/Image8.png'
+// import Image9 from '../../assets/images/Image9.png'
+// import Image10 from '../../assets/images/Image10.png'
+
+
+// export default function Projet({ images }) {
+//   return (
+//     <div className="projet-container">
+//       {images.map((image, index) => (
+//         <img
+//           key={index}
+//           src={require(`./images/${image}`)}
+//           alt={`Projet ${index + 1}`}
+//           className="projet-image"
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+// Projet.js
+
 import React from 'react';
 import './projet.scss';
-import Image1 from '../../assets/images/Image1.png'
-import Image2 from '../../assets/images/Image2.png'
-import Image3 from '../../assets/images/Image3.png'
-import Image4 from '../../assets/images/Image4.png'
-import Image5 from '../../assets/images/Image5.png'
-import Image6 from '../../assets/images/Image6.png'
-import Image7 from '../../assets/images/Image7.png'
-import Image8 from '../../assets/images/Image8.png'
-import Image9 from '../../assets/images/Image9.png'
-import Image10 from '../../assets/images/Image10.png'
-
 
 export default function Projet({ images }) {
   return (
@@ -18,7 +37,7 @@ export default function Projet({ images }) {
       {images.map((image, index) => (
         <img
           key={index}
-          src={require(`./images/${image}`)}
+          src={image}
           alt={`Projet ${index + 1}`}
           className="projet-image"
         />
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,6 +3,17 @@ import Presentation from '../../components/presentation/Presentation';
 import Competences from '../../components/competences/Competences';
 import Projet from '../../components/Projet/Projet';
 import './home.scss';
+import Image1 from '../../assets/images/Image1.png'
+import Image2 from '../../assets/images/Image2.png'
+import Image3 from '../../assets/images/Image3.png'
+import Image4 from '../../assets/images/Image4.png'
+import Image5 from '../../assets/images/Image5.png'
+import Image6 from '../../assets/images/Image6.png'
+import Image7 from '../../assets/images/Image7.png'
+import Image8 from '../../assets/images/Image8.png'
+import Image9 from '../../assets/images/Image9.png'
+import Image10 from '../../assets/images/Image10.png'
+
 
 export default function HomePage() {
   const competences = [
@@ -28,11 +39,25 @@ export default function HomePage() {
     { name: 'Visual Studio Code', url: 'https://code.visualstudio.com/' },
   ];
 
+  const images = [
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image5,
+    Image6,
+    Image7,
+    Image8,
+    Image9,
+    Image10
+  ];
+
   
 
   return (
     <div>
       <Presentation />
+      <Projet images={images} /> {/* Utilisez le composant Projet en passant les images en tant que prop */}
       <Competences competences={competences} />
     </div>
   );
