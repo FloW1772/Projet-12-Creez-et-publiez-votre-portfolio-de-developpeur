1e688b7d224a108651491a454de71c3923bea990
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-23 19:15:37 +0200

    Mis en place des images

--- src/components/Projet/Projet.jsx
+++ src/components/Projet/Projet.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import './projet.scss';
+import Image1 from '../../assets/images/Image1.png'
+import Image2 from '../../assets/images/Image2.png'
+import Image3 from '../../assets/images/Image3.png'
+import Image4 from '../../assets/images/Image4.png'
+import Image5 from '../../assets/images/Image5.png'
+import Image6 from '../../assets/images/Image6.png'
+import Image7 from '../../assets/images/Image7.png'
+import Image8 from '../../assets/images/Image8.png'
+import Image9 from '../../assets/images/Image9.png'
+import Image10 from '../../assets/images/Image10.png'
+
 
 export default function Projet({ images }) {
   return (
